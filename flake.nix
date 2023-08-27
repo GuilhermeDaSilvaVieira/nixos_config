9e2321abeda05321e28359356705ad7475d9a3b7
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-08-26 21:24:18 -0300

    sort system name

--- flake.nix
+++ flake.nix
@@ -31,11 +31,11 @@
     let
       inherit (self) outputs;
       forAllSystems = nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
         "aarch64-linux"
         "i686-linux"
-        "x86_64-linux"
-        "aarch64-darwin"
         "x86_64-darwin"
+        "x86_64-linux"
       ];
     in {
       # Your custom packages
