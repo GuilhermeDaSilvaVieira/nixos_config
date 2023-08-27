c3839191b6bc030037cf5d777531c323108a7c44
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-08-27 16:17:28 -0300

    remove unnecessary git

--- home_manager/users/enma/home.nix
+++ home_manager/users/enma/home.nix
@@ -63,9 +63,8 @@
   # programs.neovim.enable = true;
   # home.packages = with pkgs; [ steam ];
 
-  # Enable home-manager and git
+  # Enable home-manager
   programs.home-manager.enable = true;
-  programs.git.enable = true;
 
   # Nicely reload system units when changing configs
   systemd.user.startServices = "sd-switch";
