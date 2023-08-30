6d90e54fd4117cbe86b10bf531727b83ee57ae42
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-08-30 09:45:42 -0300

    update mime with librewolf and helix

--- nixos/xdg.nix
+++ nixos/xdg.nix
@@ -6,10 +6,16 @@
     };
     icons.enable = true;
     mime.defaultApplications = {
-      "text/plain" = "nvim.desktop";
+      # "text/plain" = "nvim.desktop";
+      "text/plain" = "Helix.desktop";
       "application/pdf" = "org.pwmt.zathura.desktop";
       "image/*" = "imv.desktop";
       "video/*" = "mpv.desktop";
+      "text/html" = "librewolf.desktop";
+      "x-scheme-handler/http" = "librewolf.desktop";
+      "x-scheme-handler/https" = "librewolf.desktop";
+      "x-scheme-handler/about" = "librewolf.desktop";
+      "x-scheme-handler/unknown" = "librewolf.desktop";
     };
   };
 }
