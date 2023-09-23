af25f1c94c0499c200cafa3c81bde7d8fed06d93
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-09-23 12:24:10 -0300

    proper indentation

--- overlays/default.nix
+++ overlays/default.nix
@@ -13,8 +13,8 @@
     # });
     discord = prev.discord.overrideAttrs( _: {
       src = builtins.fetchTarball {
-      url = "https://discord.com/api/download?platform=linux&format=tar.gz";
-      sha256 = "0pml1x6pzmdp6h19257by1x5b25smi2y60l1z40mi58aimdp59ss";
+        url = "https://discord.com/api/download?platform=linux&format=tar.gz";
+        sha256 = "0pml1x6pzmdp6h19257by1x5b25smi2y60l1z40mi58aimdp59ss";
       };
     });
     awesome = inputs.nixpkgs-f2k.packages.${final.system}.awesome-luajit-git;
