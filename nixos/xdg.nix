3cacc2973fffab79402b4f4c647e1b7edc002fbd
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-10-01 18:58:40 -0300

    add more entries to prevent chromium taking over mimelist

--- nixos/xdg.nix
+++ nixos/xdg.nix
@@ -8,16 +8,28 @@ xdg = {
   mime = {
     enable = true;
     defaultApplications = {
-      # "text/plain" = "nvim.desktop";
       "text/*" = "Helix.desktop";
+      "text/plain" = "Helix.desktop";
       "application/pdf" = "org.pwmt.zathura.desktop";
+      "application/rdf+xml" = "org.pwmt.zathura.desktop";
+      "application/rss+xml" = "org.pwmt.zathura.desktop";
+      "application/xhtml+xml" = "org.pwmt.zathura.desktop";
+      "application/xhtml_xml" = "org.pwmt.zathura.desktop";
+      "application/xml" = "org.pwmt.zathura.desktop";
       "image/*" = "imv.desktop";
+      "image/png" = "imv.desktop";
+      "image/jpeg" = "imv.desktop";
+      "image/gif" = "imv.desktop";
+      "image/webp" = "imv.desktop";
       "video/*" = "mpv.desktop";
       "text/html" = "librewolf.desktop";
+      "text/xml" = "librewolf.desktop";
       "x-scheme-handler/http" = "librewolf.desktop";
       "x-scheme-handler/https" = "librewolf.desktop";
       "x-scheme-handler/about" = "librewolf.desktop";
       "x-scheme-handler/unknown" = "librewolf.desktop";
+      "x-scheme-handler/mailto" = "librewolf.desktop";
+      "x-scheme-handler/webcal" = "librewolf.desktop";
       }; 
     };
   };
