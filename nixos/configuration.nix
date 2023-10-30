48fee55cdb3b6e3cdd3b2a162f0ebf7c5cc33470
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2023-10-30 11:05:34 -0300

    use release version of river

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -44,7 +44,7 @@
 
       (final: prev: { 
         awesome = inputs.nixpkgs-f2k.packages.${final.system}.awesome-luajit-git;
-        river = inputs.nixpkgs-f2k.packages.${final.system}.river-git;
+        # river = inputs.nixpkgs-f2k.packages.${final.system}.river-git;
 
         discord = prev.discord.overrideAttrs( _: {
           src = builtins.fetchTarball {
