5a44145ecb31d9dc17c815e9e71f41baf3612c66
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2024-01-13 14:40:07 -0300

    update insecurePackages

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -74,7 +74,7 @@
     config = {
       # Disable if you don't want unfree packages
       allowUnfree = true;
-      permittedInsecurePackages = lib.optional (pkgs.obsidian.version == "1.4.16") "electron-25.9.0";
+      permittedInsecurePackages = lib.optional (pkgs.obsidian.version == "1.5.3") "electron-25.9.0";
     };
   };
 
