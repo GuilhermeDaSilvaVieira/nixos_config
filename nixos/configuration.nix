43a2acb7c062b3c37fa3dae1b7d026333366b12b
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2024-01-21 12:23:23 -0300

    resume from suspension with color temperature active

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -101,6 +101,11 @@
     };
   };
 
+  powerManagement.resumeCommands = ''
+    kill wlsunset
+    wlsunset -t 3400 -T 3401
+  '';
+
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
 }
