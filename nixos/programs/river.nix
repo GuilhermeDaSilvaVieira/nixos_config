534dbe1388d1ca097b4d7ce91f0780e63dbe6812
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2024-02-01 12:00:14 -0300

    remove unused

--- nixos/programs/river.nix
+++ nixos/programs/river.nix
@@ -2,13 +2,10 @@
   programs.river = {
     enable = true;
     extraPackages = with pkgs; [
-      ristate
-      kile-wl
       rofi-wayland
       wlr-randr
       kanshi
       eww-wayland
-      mpvpaper
       swaybg
       imv
       mako
