5e309c28bdcb350c1a57441245dc59fab3ba426f
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2024-02-13 14:42:00 -0300

    add wgpu devshell

--- shell.nix
+++ shell.nix
@@ -7,4 +7,27 @@
     NIX_CONFIG = "experimental-features = nix-command flakes";
     nativeBuildInputs = with pkgs; [ nix home-manager git ];
   };
+  wgpu = pkgs.mkShell {
+    nativeBuildInputs = with pkgs; [
+      libxkbcommon
+      libGL
+      # WINIT_UNIX_BACKEND=wayland
+      wayland
+    ];
+    shellHook = ''
+      LD_LIBRARY_PATH="''${LD_LIBRARY_PATH:+$LD_LIBRARY_PATH:}${
+        with pkgs;
+          lib.makeLibraryPath [
+            vulkan-loader
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr
+          ]
+      }"
+      export LD_LIBRARY_PATH
+    '';
+    LD_LIBRARY_PATH = "${pkgs.libxkbcommon}/lib:${pkgs.libGL}/lib:${pkgs.wayland}/lib";
+    RUST_BACKTRACE=1;
+  };
 }
