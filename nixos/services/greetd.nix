ca5b0be1adefa9ea42f1a550d19481a0779fb1aa
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2024-05-06 01:10:59 -0300

    use startx instead of river as default

--- nixos/services/greetd.nix
+++ nixos/services/greetd.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --user-menu -t -c river";
+        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --user-menu -t -c startx";
       };
     };
   };
