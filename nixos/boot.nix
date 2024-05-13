c3a01db389fe0b22f90746d0edafc7883e721f07
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2024-05-13 15:16:11 -0300

    encrypt storage

--- nixos/boot.nix
+++ nixos/boot.nix
@@ -8,12 +8,15 @@
       "nvidia_uvm"
       "nvidia_drm"
     ];
-    initrd.kernelModules = [
-      "nvidia"
-      "nvidia_modeset"
-      "nvidia_uvm"
-      "nvidia_drm"
-    ];
+    initrd = {
+      luks.devices."luks-442ae00d-fe6f-4bd9-9b0e-1feff1fb2ffb".device = "/dev/disk/by-uuid/442ae00d-fe6f-4bd9-9b0e-1feff1fb2ffb";
+      kernelModules = [
+        "nvidia"
+        "nvidia_modeset"
+        "nvidia_uvm"
+        "nvidia_drm"
+      ];
+    };
     blacklistedKernelModules = [ "kms" ];
     loader = {
       systemd-boot.enable = true;
