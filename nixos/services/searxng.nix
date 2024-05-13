b5809420e6aff0994649725bf22df638f3272c82
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2024-05-13 15:21:50 -0300

    make it work with a .env file

--- nixos/services/searxng.nix
+++ nixos/services/searxng.nix
@@ -1,5 +1,10 @@
 { ... }: {
   services.searx = {
     enable = true;
+    settings = {
+      server.port = 8080;
+      server.bind_address = "0.0.0.0";
+      server.secret_key = "@SEARX_SECRET_KEY@";
+    };
   };
 }
