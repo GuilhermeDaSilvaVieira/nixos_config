6855bc025e7cd1dddc008a328fd37676c42c0d33
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2024-05-15 11:52:44 -0300

    fix flake location

--- nixos/programs/nh.nix
+++ nixos/programs/nh.nix
@@ -4,6 +4,6 @@
     enable = true;
     clean.enable = true;
     clean.extraArgs = "--keep-since 7d --keep 5";
-    flake = "/home/user/franky";
+    flake = "/home/user/franky/nixos_config";
   };
 }
