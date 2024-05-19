cbb59d7a54273ccc20e100311f1cb08f59a1eb53
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2024-05-19 11:14:25 -0300

    automate sync between android and nixos

--- nixos/services/syncthing.nix
+++ nixos/services/syncthing.nix
@@ -8,5 +8,18 @@ in
     user = user;
     dataDir = "/home/${user}/Documents/Obsidian";
     configDir = "/home/${user}/Documents/Obsidian/.config/syncthing";
+    overrideDevices = true;
+    overrideFolders = true;
+    settings = {
+      devices = {
+        "android" = { id = "PPFPPFR-KGPXPKP-HZP3K3Q-GM6FIG7-MG7UYNB-VSPDGYL-D4FCD2T-4TQPZQR"; };
+      };
+      folders = {
+        "Obsidian" = {
+          path = "/home/${user}/Documents/Obsidian";
+          devices = [ "android" ];
+        };
+      };
+    };
   };
 }
