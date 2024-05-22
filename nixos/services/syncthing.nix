690257f361dd65616bc43a991274f28dc302dc95
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2024-05-22 17:12:00 -0300

    automate syncthing with android id

--- nixos/services/syncthing.nix
+++ nixos/services/syncthing.nix
@@ -12,7 +12,9 @@ in
     overrideFolders = true;
     settings = {
       devices = {
-        "android" = { id = "PPFPPFR-KGPXPKP-HZP3K3Q-GM6FIG7-MG7UYNB-VSPDGYL-D4FCD2T-4TQPZQR"; };
+        "android" = {
+          id = "PPFPPFR-KGPXPKP-HZP3K3Q-GM6FIG7-MG7UYNB-VSPDGYL-D4FCD2T-4TQPZQR";
+        };
       };
       folders = {
         "Obsidian" = {
