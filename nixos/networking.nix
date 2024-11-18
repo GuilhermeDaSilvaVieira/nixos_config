c3a2f31c50292a38b8c79eb4975077c1181e5a71
Guilherme Da Silva Vieira <guilhermedas.vieira@gmail.com>
2024-11-18 13:06:41 -0300

    remove unnecessary firewall options

--- nixos/networking.nix
+++ nixos/networking.nix
@@ -3,24 +3,5 @@
   networking = {
     hostName = "mugiwara";
     networkmanager.enable = true;
-
-    firewall = {
-      allowedTCPPorts = [
-        # Syncthing
-        8384
-        22000
-
-        # LocalSend
-        53317
-      ];
-      allowedUDPPorts = [
-        # Syncthing
-        22000
-        21027
-
-        # LocalSend
-        53317
-      ];
-    };
   };
 }
